a0cb67a4b2fdcfed7e7f3b4656a4f8d8410d2ced
ganeshpalar17 <ganeshpalar17@gmail.com>
2025-08-28 11:57:44 +0530

    Complete

---
+++ llm_helper.py
@@ -0,0 +1,9 @@
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="meta-llama/llama-4-scout-17b-16e-instruct",
+)
